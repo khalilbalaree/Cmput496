06ff42814651903b1fe5563d3aaf342d7c3c1e6c
zijunualberta <zijun4@ualbertaclear.ca>
2019-01-12 23:27:51 -0700

    Resources at the begining of the course

---
+++ go2d.py
@@ -0,0 +1,14 @@
+# go2d.py - sketch of Go board implemented as a list of lists in Python
+# Cmput 496 sample code
+# Written by Martin Mueller
+
+EMPTY = 0
+BLACK = 1
+WHITE = 2   
+MAXSIZE = 7
+
+board = [[EMPTY for x in range(MAXSIZE)] 
+                for y in range(MAXSIZE)]
+print(board)
+board[3][4] = BLACK
+print(board)
